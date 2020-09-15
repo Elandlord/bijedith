a46744cf3762578854fb2d8c568aa768193295a8
Elandlord <e.landheer@mentechmedia.nl>
2020-09-15 08:54:20 +0200

    :rainbow: Add Google Analytics

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,5 +1,14 @@
 <html>
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-86938202-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-86938202-2');
+    </script>
     <meta charset="utf-8">
     <link rel="icon" href="/favicon.png">
     <meta name="theme-color" content="#67B7D0">

45f47494a41ebdc3540de20a35603f710c5af676
Elandlord <e.landheer@mentechmedia.nl>
2020-10-12 09:08:11 +0200

    :rainbow: Pedicure remove extra advice

--- resources/views/pages/homepage/partials/pedicure.blade.php
+++ resources/views/pages/homepage/partials/pedicure.blade.php
@@ -106,15 +106,6 @@
                                             <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
                                         </svg>
                                     </span>
-                                    Controleer de binnenkant van je schoenen op oneffenheden
-                                </li>
-                                <li class="flex items-start">
-                                    <span class="mr-1">
-                                        <svg class="w-5 h-5 mt-px text-bijedith-fresh-blue"  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                            <polyline points="9 11 12 14 22 4"></polyline>
-                                            <path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path>
-                                        </svg>
-                                    </span>
                                     Wissel indien mogelijk dagelijks je schoenen
                                 </li>
                                 <li class="flex items-start">

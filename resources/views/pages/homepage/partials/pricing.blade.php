21e8ce00cea0674962294f28a96e5d6564361bcc
Eric Landheer <eric@peterprint.nl>
2025-01-25 10:08:51 +0100

    Price indexation

--- resources/views/pages/homepage/partials/pricing.blade.php
+++ resources/views/pages/homepage/partials/pricing.blade.php
@@ -4,7 +4,7 @@
             <div class="col-lg-10 col-md-10 m-auto">
                 <div class="sec-heading">
                     <h3 class="sec-title blue-border">Tarieven</h3>
-{{--                    <p>Bij Edith verzorgen wij verschillende voetbehandelingen. Naast pedicures zijn wij compleet uitgerust met spabehandelingen.</p>--}}
+                    {{--                    <p>Bij Edith verzorgen wij verschillende voetbehandelingen. Naast pedicures zijn wij compleet uitgerust met spabehandelingen.</p>--}}
                 </div>
             </div>
         </div>
@@ -19,26 +19,27 @@
                         </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Pedicurebehandeling (standaard/medisch)</td>
-                                <td> € 38,- </td>
-                            </tr>
-                            <tr>
-                                <td>Pedicurebehandeling (kort)</td>
-                                <td> € 30,-</td>
-                            </tr>
-                            <tr>
-                                <td>Spa-arrangement - Klassieke voet- en onderbeenmassage</td>
-                                <td>€ 38,-</td>
-                            </tr>
-                            <tr>
-                                <td>Spa-arrangement - Klassieke voet- en onderbeenmassage, aansluitend aan een behandeling</td>
-                                <td>€ 25,-</td>
-                            </tr>
-                            <tr>
-                                <td>Spa-arrangement - Sparkling – p.p.</td>
-                                <td>€ 60,-</td>
-                            </tr>
+                        <tr>
+                            <td>Pedicurebehandeling (standaard/medisch)</td>
+                            <td>€ 39,50</td>
+                        </tr>
+                        <tr>
+                            <td>Pedicurebehandeling (kort)</td>
+                            <td>€ 31,50</td>
+                        </tr>
+                        <tr>
+                            <td>Spa-arrangement - Klassieke voet- en onderbeenmassage</td>
+                            <td>€ 39,50</td>
+                        </tr>
+                        <tr>
+                            <td>Spa-arrangement - Klassieke voet- en onderbeenmassage, aansluitend aan een behandeling
+                            </td>
+                            <td>€ 31,50</td>
+                        </tr>
+                        <tr>
+                            <td>Spa-arrangement - Sparkling – p.p.</td>
+                            <td>€ 70,-</td>
+                        </tr>
                         </tbody>
                     </table>
                 </div>

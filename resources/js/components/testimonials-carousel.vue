d123b9c2db0a8167f69c055615a92c0b43f488f9
Eric Landheer <eric@peterprint.nl>
2026-02-15 18:13:26 +0000

    Replace owl carousel with scroll-snap Vue components

--- resources/js/components/testimonials-carousel.vue
+++ resources/js/components/testimonials-carousel.vue
@@ -1,27 +1,125 @@
 <template>
-    <carousel :items="items" :autoplay="autoplay" :nav="nav">
-        <div class="single-tst" v-for="n in 3">
-            <img data-src="/assets/images/quote.png" class="lazyload" style="width: 40px;" alt="" />
-            <p>Ik werd zeer gastvrij ontvangen in een mooie, verzorgde spa.</p>
-            <div class="client-info">
-                <img data-src="/assets/images/person_1.jpg" alt="" class="thumb lazyload" />
-                <p>Eric Landheer, <span>software-ontwikkelaar</span></p>
-            </div>
+    <div class="testimonial-carousel">
+        <div ref="track" class="testimonial-track">
+            <article
+                v-for="(slide, index) in slides"
+                :key="index"
+                class="single-tst testimonial-slide"
+                :class="{ 'is-active': index === activeIndex }"
+            >
+                <img data-src="/assets/images/quote.png" class="lazyload quote" alt="" />
+                <p>{{ slide.quote }}</p>
+                <div class="client-info">
+                    <img data-src="/assets/images/person_1.jpg" alt="" class="thumb lazyload" />
+                    <p>{{ slide.author }}, <span>{{ slide.role }}</span></p>
+                </div>
+            </article>
         </div>
-    </carousel>
+    </div>
 </template>
+
 <script>
-    import carousel from 'vue-owl-carousel2'
+export default {
+    data() {
+        return {
+            activeIndex: 0,
+            autoplayMs: 5500,
+            intervalId: null,
+            slides: [
+                {
+                    quote: 'Ik werd zeer gastvrij ontvangen in een mooie, verzorgde spa.',
+                    author: 'Eric Landheer',
+                    role: 'software-ontwikkelaar',
+                },
+                {
+                    quote: 'Rust, vakmanschap en persoonlijke aandacht in elke behandeling.',
+                    author: 'Anonieme client',
+                    role: 'vaste klant',
+                },
+                {
+                    quote: 'Een fijne plek waar je echt met lichte voeten naar buiten loopt.',
+                    author: 'Anonieme client',
+                    role: 'bezoeker',
+                },
+            ],
+        };
+    },
 
-    export default {
-        components: { carousel },
+    mounted() {
+        this.startAutoplay();
+    },
 
-        data() {
-            return {
-                items: 1,
-                autoplay: true,
-                nav: false,
+    beforeDestroy() {
+        this.stopAutoplay();
+    },
+
+    methods: {
+        startAutoplay() {
+            if (this.slides.length <= 1) {
+                return;
             }
-        }
-    }
+
+            this.intervalId = window.setInterval(() => {
+                this.activeIndex = this.activeIndex >= this.slides.length - 1 ? 0 : this.activeIndex + 1;
+                this.scrollToActive();
+            }, this.autoplayMs);
+        },
+
+        stopAutoplay() {
+            if (!this.intervalId) {
+                return;
+            }
+
+            window.clearInterval(this.intervalId);
+            this.intervalId = null;
+        },
+
+        scrollToActive() {
+            const track = this.$refs.track;
+
+            if (!track) {
+                return;
+            }
+
+            const firstChild = track.children[0];
+
+            if (!firstChild) {
+                return;
+            }
+
+            track.scrollTo({
+                behavior: 'smooth',
+                left: firstChild.clientWidth * this.activeIndex,
+            });
+        },
+    },
+};
 </script>
+
+<style scoped>
+.testimonial-track {
+    display: flex;
+    overflow-x: auto;
+    scroll-behavior: smooth;
+    scroll-snap-type: x mandatory;
+}
+
+.testimonial-slide {
+    flex: 0 0 100%;
+    opacity: 0.45;
+    scroll-snap-align: start;
+    transition: opacity 0.25s ease;
+}
+
+.testimonial-slide.is-active {
+    opacity: 1;
+}
+
+.quote {
+    width: 40px;
+}
+
+.testimonial-track::-webkit-scrollbar {
+    display: none;
+}
+</style>
